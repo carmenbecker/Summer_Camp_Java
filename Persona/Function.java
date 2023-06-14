180a6c5d09825259bf6f1b0ef0141c4fc5aaa23d
carmenbecker <100791224+carmenbecker@users.noreply.github.com>
2023-06-14 10:24:04 +0200

    Update Function.java

--- Persona/Function.java
+++ Persona/Function.java
@@ -1,5 +1,76 @@
-package Persona;
+package Persona; 
 
 public class Function {
     
+    /*
+    * 3. 
+    * Crea dos objetos diferentes de Persona 
+    * Inspirate: Javier Lopez de Murcia que tiene 80 y 
+    * María Gonzalez de Barcelona que tiene 10)
+    */
+
+
+    /*
+     * 4. 
+     * Crea una función que reciba como parámetros los 
+     * dos objetos anteriores y se utilice para saludar 
+     * una persona a otra mediante el nombre
+     */
+
+     public String saludo(Person a, Person b) {
+        return ""; 
+     }
+
+
+    /*
+     * 5.
+     * Función para comprobar mayoría de edad
+     */
+     public boolean MayoriaDeEdad(Person a){
+        return false; 
+     }
+
+    /*
+     * 6. 
+     * Función que te devuelve un string con la información de la persona
+     */
+    public String myInfo(Person a){
+        return ""; 
+    }
+
+    /*
+     * 7. 
+     * Función para comprobar que los objetos tienen DNI diferente
+     */
+
+
+    /*
+     * 8. 
+     * Función para comparar edades de dos objetivos e imprimir 
+     * string con la info correspondiente 
+     * Ejemplo: Maria es 5 años mayor que Pepe
+     */
+
+
+    /*
+     * 9. 
+     * Crea un Array con 5 nuevos objetos (que hayas creado tú)
+     * junto a los dos objetos facilitados anteriormente.
+     */
+
+
+    /*
+    * 10.
+    * Crea un diccionario vacío que toma como key y value un String 
+    * Recorre el Array creado para añadir al diccionario las ciudades 
+    * como llaves y las personas correspondientes a cada ciudad como valores. 
+    * IMPORTANTE: Valores únicos como llaves. Imprime el diccionario al finalizar el bucle.
+    */ 
+
+
+    /*
+     * EXTRA: calcula edad a partir de la fecha de nacimiento 
+     * tendras que encontrar algunas librerías online SIN usar ChatGPT  
+     */
+
 }
